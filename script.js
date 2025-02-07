fb6bb8dba31a94530e0adf5faa219aedd7eab4b4
Avanish Singh <107432071+avanishsinghav@users.noreply.github.com>
2025-02-07 22:52:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let result = " ";
+	const characterlength = parse(l);
+	for(let  = 0; i<characterlen; i++){
+		result += characters.charAt(Math.floor(Math.random()*characterlength));
+	}
+	return result;
 }
 
 // Do not change the code below.
